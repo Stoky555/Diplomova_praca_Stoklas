b3368a056a540a1b08ac611b5f700cf869262b55
gabri <gabri@STOKY-PC>
2024-05-04 17:30:03 +0200

    final stages of diplomArbeit

--- Assets/Scripts/ScannerScene/PopulateDropdownController.cs
+++ Assets/Scripts/ScannerScene/PopulateDropdownController.cs
@@ -44,47 +44,4 @@ public class PopulateDropdownController : MonoBehaviour
         dropdown.ClearOptions();
         dropdown.AddOptions(options);
     }
-
-    ////public void PopulateDropdownWithXMLFiles()
-    ////{
-    ////    string path = Path.Combine(Application.streamingAssetsPath, "Vuforia");
-    ////    List<string> options = new List<string>();
-
-    ////    // Verify the directory exists
-    ////    if (!Directory.Exists(path))
-    ////    {
-    ////        Debug.LogWarning($"PopulateDropdown: Directory does not exist at path: {path}");
-    ////        return;
-    ////    }
-
-    ////    try
-    ////    {
-    ////        // Get all XML files in the directory
-    ////        string[] files = Directory.GetFiles(path, "*.xml");
-    ////        if (files.Length == 0)
-    ////        {
-    ////            Debug.LogWarning("PopulateDropdown: No XML files found in the directory.");
-    ////            return;
-    ////        }
-
-    ////        options.Add("Choose database");
-
-    ////        // Extract and add file names (without extension) to the options list
-    ////        foreach (string file in files)
-    ////        {
-    ////            options.Add(Path.GetFileNameWithoutExtension(file));
-    ////        }
-
-    ////        // Populate the dropdown with these options
-    ////        dropdown.ClearOptions();
-    ////        dropdown.AddOptions(options);
-
-    ////        databases = options;
-    ////    }
-    ////    catch (System.Exception ex)
-    ////    {
-    ////        Debug.LogError($"PopulateDropdown: Failed to read XML files from directory. Error: {ex.Message}");
-    ////        databases = new List<string>();
-    ////    }
-    ////}
 }
